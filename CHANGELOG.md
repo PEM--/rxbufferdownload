672fd8ece2acb717d28e02934c8287fe020923f7
PEM-- <pemarchandet@gmail.com>
2015-11-02 19:32:58 +0100

    Meteor 1.2.1 & Package bumped

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 0.1.0 - Meteor 1.2.1
+* Package bumped.
+
 # 0.0.1 - Initial package
 * Simple reactive binary downloader.
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'pierreeric:rxbufferdownload',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Reactive binary downloader',
   // URL to the Git repository containing the source code for this package.
@@ -11,14 +11,14 @@ Package.describe({
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.2');
+  api.versionsFrom('1.2.1');
   // Required packages
   api.use([
     // Package from MDG
     'coffeescript',
     'reactive-var',
     // Community packages
-    'mquandalle:bower@1.4.1'
+    'mquandalle:bower@1.5.2_1'
   ]);
   // Imported files
   api.addFiles([
